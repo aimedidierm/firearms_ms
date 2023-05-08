de8b2463eaf3ede9917f7c51b506ff2ef24add78
aimedidierm <aimedidiermugisha@gmail.com>
2023-05-08 11:52:57 +0300

    Database & models

---
+++ app/Http/Controllers/PsychiatricController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Psychiatric;
+use Illuminate\Http\Request;
+
+class PsychiatricController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Psychiatric $psychiatric)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Psychiatric $psychiatric)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Psychiatric $psychiatric)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Psychiatric $psychiatric)
+    {
+        //
+    }
+}
