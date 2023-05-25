2e2283a72ca44df03bb24aa7acdd3333b2ac0937
aimedidierm <aimedidiermugisha@gmail.com>
2023-05-25 21:06:10 +0000

    Production error

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,27 +4,30 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Illuminate\Http\Exceptions\PostTooLargeException;
+use Illuminate\Http\Response;
 
 class Handler extends ExceptionHandler
 {
-    /**
-     * The list of the inputs that are never flashed to the session on validation exceptions.
-     *
-     * @var array<int, string>
-     */
     protected $dontFlash = [
         'current_password',
         'password',
         'password_confirmation',
     ];
 
-    /**
-     * Register the exception handling callbacks for the application.
-     */
-    public function register(): void
+    public function register()
     {
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $exception)
+    {
+        if ($exception instanceof PostTooLargeException) {
+            return response()->json(['error' => 'The file you uploaded is too large.'], Response::HTTP_REQUEST_ENTITY_TOO_LARGE);
+        }
+
+        return parent::render($request, $exception);
     }
 }
